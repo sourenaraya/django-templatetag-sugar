f5434d966f28a66460d47040ff7c81a894b52e4e
sourenaraya <sourenaraya@users.noreply.github.com>
2016-05-02 01:55:11 +1000

    Djnango 1.9 support

--- templatetag_sugar/parser.py
+++ templatetag_sugar/parser.py
@@ -1,7 +1,10 @@
 from collections import deque
 from copy import copy
 
-from django.db.models.loading import cache
+try:
+    from django.db.models.loading import cache
+except ImportError:
+    from django.apps import apps as cache
 from django.template import TemplateSyntaxError
 
 from templatetag_sugar.node import SugarNode
